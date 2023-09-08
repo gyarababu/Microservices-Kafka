5395acd788161e2425e61ee6edada8ec0a4fe58b
Gyarababu <gyarababu9@gmail.com>
2023-09-08 15:22:06 +0530

    Initial commit for all 4 MicroServices using Kafka

---
+++ email-microservice/src/main/java/com/springboot/email/EmailMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.email;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailMicroserviceApplication.class, args);
+	}
+
+}
